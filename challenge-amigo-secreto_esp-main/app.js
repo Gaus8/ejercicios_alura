3cddb80d52be21ad35a00fc381a91b93f041c1d0
Stiven Garzon <usg210802@outlook.com>
2025-03-16 17:25:07 -0500

    Creacion de funcion sortear amigos y documentacion de código

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -3,13 +3,18 @@
 //Declaracion de variables
 const input = document.getElementById('amigo');
 const listaAmigos = document.getElementById('listaAmigos');
+const resultado = document.getElementById('resultado');
 const arregloAmigos = [];
 
 //Funcion para agregar amigos
 function agregarAmigo() {
   //Se quitan los espacios en blancos, en caso de que se presenten.
   const nombreAmigo = input.value.trim();
+  //Se valida que el input este vacio
+  if(!nombreAmigo) return window.alert('No se puede enviar un nombre vacio')
+  //Se añade el amigo al arreglo
   arregloAmigos.push(nombreAmigo);
+  //Se limpia el <ul> para añadir y mostrar los nuevos elementos
   listaAmigos.innerHTML = ''
   //Se recorre el arreglo con un forEach, añadiendo los nuevos nombres
   arregloAmigos.forEach(amigo =>{
@@ -17,6 +22,14 @@ function agregarAmigo() {
   })
 }
 
-
+//Funcion para sortear amigos
+function sortearAmigo() {
+  //Se coloca en una constante la longitud del arreglo
+  const longitud = arregloAmigos.length;
+  //Se genera un numero aleatorio que sera usado para colocarlo en la posicion del arreglo
+  let random = Math.floor(Math.random() * longitud) + 1;
+  //Se muestra el amigo sorteado, se le resta uno para no sobrepasar el límite del arreglo
+  resultado.innerHTML = `<li>Tu amigo secreto es: ${arregloAmigos[random - 1]}</li>`;
+}
 
 
