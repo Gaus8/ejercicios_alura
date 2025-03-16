1b02a908f4d679658109e4030aa96b645cb934c3
Stiven Garzon <usg210802@outlook.com>
2025-03-16 17:13:06 -0500

    Declaracion de variables y creacion de la funcion para añadir amigos

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,22 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Declaracion de variables
+const input = document.getElementById('amigo');
+const listaAmigos = document.getElementById('listaAmigos');
+const arregloAmigos = [];
+
+//Funcion para agregar amigos
+function agregarAmigo() {
+  //Se quitan los espacios en blancos, en caso de que se presenten.
+  const nombreAmigo = input.value.trim();
+  arregloAmigos.push(nombreAmigo);
+  listaAmigos.innerHTML = ''
+  //Se recorre el arreglo con un forEach, añadiendo los nuevos nombres
+  arregloAmigos.forEach(amigo =>{
+    listaAmigos.innerHTML += `<li>${amigo}</li>`;
+  })
+}
+
+
+
+
